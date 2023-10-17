bab529b37bc6c1592e12c20c85c72a03cac13ebd
BabyBo <35010886+BabyBoChen@users.noreply.github.com>
2023-10-17 08:33:02 +0800

    Update secret.dart

--- bin/secrets/secret.dart
+++ bin/secrets/secret.dart
@@ -3,7 +3,6 @@ library visit_counter.secrets;
 import 'dart:io';
 
 class Secret {
-  //static const String MONGODB_CONNSTRING = "mongodb+srv://sa:75whVEi8ei1XvLNj@bblj.76jhfiu.mongodb.net/visitor_count?retryWrites=true&w=majority";
   static String get MONGODB_CONNSTRING {
     return Platform.environment["MONGODB_CONNSTRING"]!;
   }
